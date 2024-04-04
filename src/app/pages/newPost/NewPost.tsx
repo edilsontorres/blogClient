4dea73ec0de6b3543e2a59e8caaa950ec67fbcb2
edilson torres <edilsontorres.job@gmail.com>
2024-04-03 21:35:07 -0300

    Enviando dados do Ckeditor para DB

--- src/app/pages/newPost/NewPost.tsx
+++ src/app/pages/newPost/NewPost.tsx
@@ -1,6 +1,8 @@
 import { useState, FormEvent } from "react";
 import { PostService } from "../../shared/services/Api/Posts/PostService";
 import { useNavigate } from "react-router-dom";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 export const NewPost = () => {
     const navigate = useNavigate();
@@ -9,7 +11,7 @@ export const NewPost = () => {
     const [postAuthor, setPostAuthor] = useState('');
 
   
-
+    
     const stopDefAction = (event:FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
@@ -28,8 +30,12 @@ export const NewPost = () => {
         }
 
     }
-
     
+    const ckEditorValue = (event:any, editor:ClassicEditor) => {
+        let data = editor.getData();
+        setPostContent(data);
+        
+    }
 
     const newPost = async (post:any) => {
         await PostService.newPost(post);
@@ -44,14 +50,22 @@ export const NewPost = () => {
                 <input type="text" onChange={(event) => setPostTitle(event.target.value)} />
 
                 <h3>Conteudo:</h3>
-                <textarea onChange={(event) => setPostContent(event.target.value)} />
+                <CKEditor
+
+                    editor={ClassicEditor}
+                    onReady={editor => {
+
+                    }}
+                    onChange={ckEditorValue}
+                />
 
                 <h3>Autor:</h3>
                 <input type="text" onChange={(event) => setPostAuthor(event.target.value)} />
 
+
                 <h3>Imagens:</h3>
                 <input type="file" name="img" />
-    
+
                 <input type="submit" value={"Enviar"} />
 
             </form>
