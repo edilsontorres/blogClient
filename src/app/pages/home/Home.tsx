28135cd32cfde262c57d263f2d338eae709132f4
edilson torres <edilsontorres.job@gmail.com>
2024-04-11 21:55:21 -0300

    Formatando HTML do Content

--- src/app/pages/home/Home.tsx
+++ src/app/pages/home/Home.tsx
@@ -1,18 +1,20 @@
 import { useEffect, useState } from "react";
 import { IPost, PostService } from "../../shared/services/Api/Posts/PostService";
 
+
+
 export const Home = () => {
 
-    const[post, setPost] = useState<IPost[]>([]);
+  const [post, setPost] = useState<IPost[]>([]);
 
-    useEffect(()=>{
-        PostService.listPost()
-        .then((result) => {
-          setPost(result);
-        });
-      }, []);
+  useEffect(() => {
+    PostService.listPost()
+      .then((result) => {
+        setPost(result);
+      });
+  }, []);
 
-    return(
+  return (
     <>
       <h1>Lista de Artigos</h1>
       <table>
@@ -21,20 +23,28 @@ export const Home = () => {
             <td>Titulo</td>
             <td>Conteudo</td>
             <td>Autor</td>
-          </tr> 
+          </tr>
         </thead>
         {post.map((post, index) =>
+
           <tbody key={index}>
             <tr>
               <td><p>{post.title}</p></td>
-              <td><p>{post.content}</p></td>
+              <td>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: (post.content)
+                  }}
+                ></div>
+              </td>
               <td><p>{post.author}</p></td>
             </tr>
           </tbody>
+
         )}
       </table>
-      
-      
+
+
     </>
   );
 }
