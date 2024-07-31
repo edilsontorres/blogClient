0f6e3ef9559691ac3099e9d5a854501476408e66
edilson <edilsontor77@gmail.com>
2024-07-30 23:04:53 -0300

    Estilo Rodapé

---
+++ src/app/shared/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import * as F from './FooterStyle';
+
+export const Footer = () => {
+    return(
+        <>
+            <footer>
+                <F.CopyrightContainer>
+                    <h3>© Copyright 2024 | Edilson Torres. Todos os Direitos Reservados</h3>
+                </F.CopyrightContainer>
+            </footer>
+        </>
+    );
+}
---
+++ src/app/shared/components/footer/FooterStyle.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const CopyrightContainer = styled.div`
+    background-color: black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 50px;
+    width: 100%;
+    margin-top: 50px;
+    padding: 20px 0px 20px 0px;
+    h3{
+        color: #FFFFFF;
+        font-family: 'Roboto';
+        font-weight: 400;
+    }
+
+`;
