63cf974764c1df24aa05ee6c795a99638b259793
edilson <edilsontor77@gmail.com>
2024-08-13 22:22:54 -0300

    Layout final

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -3,27 +3,25 @@ import { IPost, PostService } from "../../shared/services/Api/Posts/PostService"
 import { useNavigate } from "react-router-dom";
 import * as D from "./DashboardStyle";
 import { Footer } from "../../shared/components/footer/Footer";
-import { DataPost } from "../../shared/components/data/dataPost";
+import { FormatData } from "../../shared/services/Data/FormatData";
+import { FaPen, FaTrashAlt, FaPlusCircle } from "react-icons/fa";
+import avatar from "../../img/avatar.png"
+import { Thumb } from "../home/thumb";
 
 export const Dashboard = () => {
-  const[post, setPost] = useState<IPost[]>([]);
+  const [post, setPost] = useState<IPost[]>([]);
   const navigate = useNavigate();
-  
 
-  useEffect(()=>{
+  useEffect(() => {
     PostService.listPost()
-    .then((result) => {
-      setPost(result);
-    });
+      .then((result) => {
+        setPost(result);
+      });
   }, []);
 
-  const dadosPost = (post:IPost) => {
-    return navigate(`/postagens`, { state:{post: post} });
-    
-  }
+  const dadosPost = (post: IPost) => {
+    return navigate(`/postagens`, { state: { post: post } });
 
-  const home = () => {
-    navigate('/')
   }
 
   const novoPost = () => {
@@ -31,75 +29,65 @@ export const Dashboard = () => {
   }
 
   const deletePost = (id: number) => {
-    if(id != null){
-        PostService.removePost(id);
-        alert("Post deletado com sucesso!");
-        return window.location.reload();
+    if (id != null) {
+      PostService.removePost(id);
+      alert("Post deletado com sucesso!");
+      return window.location.reload();
     }
-    
-    
-}
+  }
 
   return (
     <>
-
       <D.dashboardHeader>
-        <h2>Dashboard - Centro de controle</h2>
+        <D.photoArea>
+          <img src={avatar} />
+        </D.photoArea>
+        <D.nameArea>Edilson Torres</D.nameArea>
       </D.dashboardHeader>
-       
-      <D.dashboardContainer>
-        <table>
-          <thead>
-            <tr>
-              <th>Data</th>
-              <th>Titulo</th>
-              <th>Ação</th>
-            </tr> 
-          </thead>
+
+      <D.dashboardContainerTitleArea>
+        <D.dashboardContainerTitle>
+          <h2>Dashboard</h2>
+          <D.actionNewPost onClick={() => novoPost()}>
+            <FaPlusCircle />
+            <p>Add New Post</p>
+          </D.actionNewPost>
+        </D.dashboardContainerTitle>
+      </D.dashboardContainerTitleArea>
+
+      <D.mainDashboard>
+        <D.dashboardContainer>
           {post.map((post, index) =>
-            <tbody key={index}>
-              <tr>
-                <td><DataPost data ={post.createdAt} /></td>
-                <td><p>{post.title}</p></td>
-                <td>
-                  <button onClick={() => dadosPost(post)}>Editar</button>
-                  <button onClick={() => deletePost(post.id)}>Excluir</button>
-                </td>
-              </tr>
-            </tbody>
-          )}
-        </table>
-      </D.dashboardContainer>
+            <D.postContainer key={index}>
+              <D.thumbPost>
+                <Thumb id={post.id} />
+              </D.thumbPost>
+              <D.tilePost>
+                <h2>{post.title}</h2>
+                <D.pArea>
+                  <D.pAuthor>
+                    <p>{post.author}</p>
+                  </D.pAuthor>
+                  <D.pData>
+                    <p>{FormatData(post.createdAt)}</p>
+                  </D.pData>
+                </D.pArea>
+              </D.tilePost>
+              <D.actionPost>
+                <D.svgArea>
+                  <FaPen onClick={() => dadosPost(post)} />
+                </D.svgArea>
+                <D.svgArea>
+                  <FaTrashAlt onClick={() => deletePost(post.id)} />
+                </D.svgArea>
+              </D.actionPost>
+            </D.postContainer>
+          )
+          }
 
-      
-
-      {/* <h1>Lista de Artigos</h1>
-      <button onClick={novoPost}>Novo Post</button>
-      <button onClick={home}>Home</button>
-      <table>
-        <thead>
-          <tr>
-            <td>Titulo</td>
-            <td>Autor</td>
-            <td>Ação</td>
-          </tr> 
-        </thead>
-        {post.map((post, index) =>
-          <tbody key={index}>
-            <tr>
-              <td><p>{post.title}</p></td>
-              <td><p>{post.author}</p></td>
-              <td>
-                <button onClick={() => dadosPost(post)}>Editar</button>
-                <button onClick={() => deletePost(post.id)}>Excluir</button>
-              </td>
-            </tr>
-          </tbody>
-        )}
-      </table> */}
+        </D.dashboardContainer>
+      </D.mainDashboard>
       <Footer />
     </>
-
-   
   );
 }
--- src/app/pages/dashboard/DashboardStyle.tsx
+++ src/app/pages/dashboard/DashboardStyle.tsx
@@ -1,33 +1,208 @@
 import styled from "styled-components";
 
+
+export const paletaCores = styled.div`
+    .um{
+        background-color: #5886FF;
+    }
+    .dois{
+        background-color: #76C6AE;
+    }
+    .tres{
+        background-color: #4494C0;
+    }
+    .quatro{
+        background-color: #F7931B;
+    }
+    .cinco{
+        background-color: #28A745;
+    }
+    .seis{
+        background-color: #0F1A34;
+    }
+    .sete{
+        background-color: #F47F60;
+    }
+    .oito{
+        background-color: #302D43;
+    }
+    .nove{
+        background-color: #F9788E;
+    }
+    .dez{
+        background-color: #77C454;
+    }
+    .onze{
+        background-color: #373063;
+    }
+    .doze{
+        background-color: #F9F9F9;
+    }
+
+`;
+
 export const dashboardHeader = styled.div`
     display: flex;
-    flex: 1;
-    background-color: #000000;
+    height: 50px;
+    background-color: #FFFFFF;
     padding: 5px;
+    justify-content: right;
+    align-items: center;
+    margin-bottom: 10px;
+`;
+
+export const nameArea = styled.div`
+    display: block;
+    color: #302D43;
+    text-align: start;
+    font-family: 'Roboto';
+    margin-right: 15px;
+`;
+
+export const photoArea = styled.div`
+    display: flex;
+    margin-right: 10px;
+    width: 40px;
+    height: 40px;
     justify-content: center;
     align-items: center;
-    color: #FFFFFF;
-    margin-bottom: 10px;
+    img {
+        width: 40px;
+        height: 40px;
+        border-radius:50px;
+    }
+
+`;
+
+export const dashboardContainerTitleArea = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100px;
+    //margin-bottom: 20px;
+    background-color: #F9F9F9;
+
+`;
+
+export const dashboardContainerTitle = styled.div`
+    min-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     h2{
+        color: #0F1A34;
         font-family: 'Roboto';
+        font-weight: 600;
+        
     }
 `;
 
-export const dashboardContainer = styled.div`
-    min-height: 100vh; /* Leva em consideração a altura do cabeçalho e do rodapé */
-    padding-bottom: 30px; /* Leva em consideração a altura do rodapé */
-    margin: 0 auto;
-    max-width: 1200px;
-    display: block;
-    table {
-        margin-top: 30px;
-        width: 100%;
-        text-align: center;
-        font-size: 20px;
-    }
-    thead{
+export const actionNewPost = styled.div`
+    display: flex;
+    width: 150px;
+    height: 35px;
+    border-radius: 40px;
+    background-color: #28A745;
+    border: none;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    padding: 4px 8px;
+    color: #FFFFFF;
+    p{
         font-family: 'Roboto';
+        font-weight: 600;
+        margin-left: 8px;
+        font-size: 15px;
+    }
+    svg{
+        font-size: 25px;
+    }
+`;
+export const mainDashboard = styled.main`
+    background-color: #F9F9F9;
+    width: 100%;
+    height: auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+`;
+export const dashboardContainer = styled.div`
+    min-height: 100vh;
+    padding-bottom: 30px;
+    margin: 0 auto;
+    padding: 20px;
+    min-width: 1200px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+`;
+export const postContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #FFFFFF;
+    margin-bottom: 10px;
+    width: 100%;
+    border: 1px solid #d3d3d3;
+`;
+export const thumbPost = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5px;
+    width: 100%;
+    img{
+        width: 200px;
+        height: 200px;
+    }
+`;
+export const tilePost = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding-top: 20px;
+    width: 100%;
+    height: 210px;
+    text-align: justify;
+    font-family: 'Roboto';
+`;
+export const pArea = styled.div`
+    display: flex;
+    margin-top: 5%;
+    width: 80%;
+    justify-content: space-between;
+    font-weight: 600;
+    color: #302D43;
+`;
+
+export const pAuthor = styled.div`
+    text-align: justify;
+`;
+export const pData = styled.div`
+    cursor: pointer;
+    :hover{
+        text-decoration: underline;
     }
 
+`;
+export const actionPost = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+`;
+export const svgArea = styled.div`
+    display: block;
+    align-items: center;
+    justify-content: center;
+    margin: 20px;
+    cursor: pointer;
+    svg{
+        width: 20px;
+        height: 20px;
+        color: #302D43;
+    }
 `;
