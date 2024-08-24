9ee1f39fdd05cef646df923fa417a2b1e315ee4c
edilson <edilsontor77@gmail.com>
2024-08-23 22:27:12 -0300

    Layout da pagina

--- src/app/pages/update/Update.tsx
+++ src/app/pages/update/Update.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom"
 import { IPost, PostService } from "../../shared/services/Api/Posts/PostService";
+import * as U from "./UpdateStyle";
+import { Footer } from "../../shared/components/footer/Footer";
+import { UploadImg } from "../../shared/components/uploadImg/UploadImg";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 export const Update = () => {
     const dadosPost = useLocation().state.post;
@@ -8,18 +13,28 @@ export const Update = () => {
     const [postTitle, setPostTitle] = useState(dadosPost.title);
     const [postContent, setPostContent] = useState(dadosPost.content);
     const [postAuthor, setPostAuthor] = useState(dadosPost.author);
-   
+    const [imgFile, setImgFile] = useState<File>();
 
+    const imgResult = (img: File) => {
+        setImgFile(img);
+    }
+   
     const dados:IPost = {
         id: dadosPost.id,
         title: postTitle,
         content: postContent,
         author: postAuthor,
+        img: imgFile!
     }
-    
+
     const stopDefAction = (event:React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         update(dadosPost.id, dados);
+    }
+
+    const ckEditorValue = (event: any, editor: ClassicEditor) => {
+        const data = editor.getData();
+        setPostContent(data);
     }
 
     const update = (id: number, dados:IPost) => {
@@ -29,25 +44,69 @@ export const Update = () => {
         } else {
             console.log("Deu azia");
         }
-        
-        
+    }
+
+    const dashboard = () => {
+        navigate('/dashboard');
     }
     
     return(
         <>
-           <form onSubmit={stopDefAction}>
-                <h3>Titulo:</h3>
-                <input type="text" defaultValue={dadosPost.title} onChange={(event) => setPostTitle(event.target.value)}/>
+            <U.container>
+                <U.containerTitleArea>
+                    <U.containerTitle>
+                        <h2>Atualizar Post</h2>
+                    </U.containerTitle>
+                </U.containerTitleArea>
+                <form onSubmit={stopDefAction}>
+                    <U.mainContainerArea>
+                        <U.containerTextArea>
+                            <U.titlePostContainer>
+                                <h2>Título</h2>
+                                <input type="text" defaultValue={dadosPost.title} onChange={(event) => setPostTitle(event.target.value)} />
+                            </U.titlePostContainer>
+                        </U.containerTextArea>
 
-                <h3>Conteudo:</h3>
-                <textarea  defaultValue={dadosPost.content} onChange={(event) => setPostContent(event.target.value)}/>
+                        <U.containerContentArea>
+                            <U.contentPostContainer>
+                                <h2>Post</h2>
+                                <CKEditor
+                                    editor={ClassicEditor}
+                                    data={dadosPost.content}
+                                    onReady={editor => {
+                                        
+                                    }}
+                                    onChange={ckEditorValue}
+                                />
+                            </U.contentPostContainer>
+                        </U.containerContentArea>
 
-                <h3>Autor:</h3>
-                <input type="text" defaultValue={dadosPost.author} onChange={(event) => setPostAuthor(event.target.value)}/>
+                        <U.containerTextArea>
+                            <U.titlePostContainer>
+                                <h2>Autor</h2>
+                                <input type="text" defaultValue={dadosPost.author} onChange={(event) => setPostAuthor(event.target.value)} />
+                            </U.titlePostContainer>
+                        </U.containerTextArea>
 
-                <input type="submit"/>
-            </form>
-
-        </> 
+                        <U.thumbContainer>
+                            <h2>Thumb do post</h2>
+                            <UploadImg img={imgResult} />
+                        </U.thumbContainer>
+                        <hr />
+                        <U.buttonArea>
+                            <U.buttonContainer>
+                                <button onClick={dashboard} className="cancel">
+                                    Cancelar
+                                </button>
+                                <button className="publish">
+                                    Atualizar
+                                </button>
+                            </U.buttonContainer>
+                        </U.buttonArea>
+                    </U.mainContainerArea>
+                </form>
+            </U.container>
+            <Footer />
+        </>
     )
 }
